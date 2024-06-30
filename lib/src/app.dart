524d96e29dfbf1d92ed84fb4177f9088e616efda
ykatakuri <yannatkk@gmail.com>
2024-06-30 10:34:30 +0200

    chore: Remove commented out code in app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
